a7e0a34f1b7cdcf432c567154c1130f19b6743a5
Jean-Luc <an.obscurity@gmail.com>
2017-02-15 22:16:48 +1100

    Added features:
    * multithreading
    * many curves
    * buffers size
    * update/refresh for plot

--- numato_adc_gui.py
+++ numato_adc_gui.py
@@ -6,6 +6,8 @@ import time
 import numpy as np
 import numato_adc as nadc
 import pyqtgraph as pg
+import threading
+import collections as col
 
 ui_classes = uic.loadUiType('./numato_adc_gui.ui')
 form_class = ui_classes[0]
@@ -21,26 +23,38 @@ class NumatoAdcGui(base_class, form_class):
         super().__init__()
         self.setupUi(self)
 
-        self.adc_plot_curve = self.adc_plot.plot()
+        self.adc_plot_curves = []
+        for i in range(6):
+            self.adc_plot_curves.append(self.adc_plot.plot())
+            setattr(self, 'adc_plot_curve_%i' % i, self.adc_plot_curves[i])
 
         self._update_plot_timer = Qt.QTimer()
         self._update_plot_timer.timeout.connect(self.update)
         self.set_plot_update_time_s()
-        self.update_time.editingFinished.connect(self.set_plot_update_time_s)
+        self.replot_time_s.editingFinished.connect(self.set_plot_update_time_s)
+        self.sample_rate_ms.editingFinished.connect(self.set_sample_rate_ms)
+        self.data_buffer_size_samples.editingFinished.connect(self.set_data_buffer_size)
         self._timer_running = True
         self.start_stop.clicked.connect(self.toggle_update_plot_timer)
 
         # set plot defaults
-        self.adc_plot.setLimits(yMin=0, yMax=5)
+        self.adc_plot.setLimits(yMin=0, yMax=10)
 
         # plotter and thread are none at the beginning
-        self.plotter = Plotter()
+        self._data_buffer_size = int(self.data_buffer_size_samples.text())
+        self._time_deque = col.deque([], self._data_buffer_size)
+        self._data_deque = col.deque([], self._data_buffer_size)
+        self.plotter = Plotter(self._data_buffer_size, float(self.sample_rate_ms.text()))
         self.thread = Qt.QThread()
 
         self.send_fig.connect(self.plotter.replot)
         self.plotter.return_fig.connect(self.plot)
         self.plotter.moveToThread(self.thread)
         self.thread.start()
+
+    def closeEvent(self, event):
+        self.plotter.get_data_flag = False
+        super(Qt.QMainWindow, self).closeEvent(event)
 
     def toggle_update_plot_timer(self):
         if self._timer_running:
@@ -51,39 +65,69 @@ class NumatoAdcGui(base_class, form_class):
             self.set_plot_update_time_s()
 
     def set_plot_update_time_s(self):
-        self._ticker = self.update_time.text()
+        self._ticker = self.replot_time_s.text()
         self._update_plot_timer.start(float(self._ticker)*1000.)
+
+    def set_sample_rate_ms(self):
+        self._sample_rate_ms = float(self.sample_rate_ms.text())
+        self.plotter.sample_rate_ms = self._sample_rate_ms
+
+    def set_data_buffer_size(self):
+        data_buffer_size = int(self.data_buffer_size_samples.text())
+        self._data_buffer_size = data_buffer_size
+        time_deque = self._time_deque
+        data_deque = self._data_deque
+        self._time_deque = col.deque(time_deque, self._data_buffer_size)
+        self._data_deque = col.deque(data_deque, self._data_buffer_size)
+        self.plotter.data_buffer_size = data_buffer_size
+        self.plotter.time_deque = col.deque(time_deque, self._data_buffer_size)
+        self.plotter.data_deque = col.deque(data_deque, self._data_buffer_size)
 
     def update(self):
         self.send_fig.emit(self._ticker)
 
     def plot(self, time, data):
-        #self.adc_plot.line.set_data([np.arange(len(data)), data])
-        self.adc_plot_curve.setData(time, data)
-        #self.adc_plot.axes.set_xlim([0, len(data)])
-        #self.adc_plot.axes.set_ylim([data.min(), data.max()])
-        #self.adc_plot.draw()
+        self._time_deque.extend(time)
+        self._data_deque.extend(data)
+        active_plots = self.get_active_plots()
+        active_idxs = np.where(active_plots)[0]
+        for idx in active_idxs:
+            self.adc_plot_curves[idx].setData(self._time_deque,
+                                              self._data_deque+idx)
+
+    def get_active_plots(self):
+        active_plots_lst = []
+        for i in range(6):
+            active_plots_lst.append(getattr(self, 'adc_%i' % i).isChecked())
+        return active_plots_lst
 
 class Plotter(Qt.QObject):
     return_fig = Qt.pyqtSignal(object, object)
 
-    def __init__(self, data_buffer_size=50):
+    def __init__(self, data_buffer_size, sample_rate_ms):
         Qt.QObject.__init__(self)
+
         self.start_time = time.time()
-        self.time_buffer = np.zeros(data_buffer_size)
-        self.data_buffer = np.zeros(data_buffer_size)
+        self.data_buffer_size = data_buffer_size
+        self.sample_rate_ms = sample_rate_ms
+        self.time_deque = col.deque([], data_buffer_size)
+        self.data_deque = col.deque([], data_buffer_size)
+        self.get_data_flag = True
+        self.data_thread = threading.Thread(target=self.get_data)
+        self.data_thread.start()
 
     def get_data(self):
-        self.time_buffer = np.roll(self.time_buffer, -1)
-        self.time_buffer[-1] = time.time() - self.start_time
-        self.data_buffer = np.roll(self.data_buffer, -1)
-        self.data_buffer[-1] = np.random.rand(1)
-        return self.time_buffer, self.data_buffer
+        while self.get_data_flag:
+            self.time_deque.append(time.time()-self.start_time)
+            self.data_deque.append(np.random.rand(1)[0])
 
     @Qt.pyqtSlot(str)
     def replot(self, ticker):
-        time, data = self.get_data()
-        self.return_fig.emit(time, data)
+        time_deque = self.time_deque
+        data_deque = self.data_deque
+        self.time_deque = col.deque([], self.data_buffer_size)
+        self.data_deque = col.deque([], self.data_buffer_size)
+        self.return_fig.emit(time_deque, data_deque)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
